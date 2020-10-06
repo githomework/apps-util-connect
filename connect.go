da875cbf431f9b72ee11373b170491ab15d04384
David Lam <david.lam@winc.com.au>
2020-10-07 09:18:46 +1100

    encryption

--- connect.go
+++ connect.go
@@ -5,22 +5,23 @@ import (
 
 	"github.com/SAP/go-hdb/driver"
 	_ "github.com/denisenkom/go-mssqldb"
+	secret "github.com/githomework/apps-util-secret"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-oci8"
 )
 
 // "<user name>/<password>@<server name>:1521/<service name>"
-func Oracle(dsn string) *sqlx.DB {
-	db, err := sqlx.Open("oci8", dsn)
+func Oracle(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open("oci8", secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Println(err)
 	}
 	return db
 }
 
-func OracleMust(dsn string) *sqlx.DB {
-	db, err := sqlx.Open("oci8", dsn)
+func OracleMust(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open("oci8", secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -28,16 +29,16 @@ func OracleMust(dsn string) *sqlx.DB {
 }
 
 // "hdb://<user name>:<password>@<server name>:<port>"
-func HANA(dsn string) *sqlx.DB {
-	db, err := sqlx.Open(driver.DriverName, dsn)
+func HANA(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open(driver.DriverName, secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Println(err)
 	}
 	return db
 }
 
-func HANAMust(dsn string) *sqlx.DB {
-	db, err := sqlx.Open(driver.DriverName, dsn)
+func HANAMust(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open(driver.DriverName, secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -45,16 +46,16 @@ func HANAMust(dsn string) *sqlx.DB {
 }
 
 // "server=<server name>;user id=<user name>;password=<password>;port=1433;database=<database name>;encrypt=disable"
-func MSSQL(dsn string) *sqlx.DB {
-	db, err := sqlx.Open("mssql", dsn)
+func MSSQL(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open("mssql", secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Println(err)
 	}
 	return db
 }
 
-func MSSQLMust(dsn string) *sqlx.DB {
-	db, err := sqlx.Open("mssql", dsn)
+func MSSQLMust(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open("mssql", secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -62,16 +63,16 @@ func MSSQLMust(dsn string) *sqlx.DB {
 }
 
 // "host=<server name> user=<user name> dbname=<database name> password=<password> sslmode=disable port=5432"
-func Postgres(dsn string) *sqlx.DB {
-	db, err := sqlx.Open("postgres", dsn)
+func Postgres(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open("postgres", secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Println(err)
 	}
 	return db
 }
 
-func PostgresMust(dsn string) *sqlx.DB {
-	db, err := sqlx.Open("postgres", dsn)
+func PostgresMust(key string, dsn string) *sqlx.DB {
+	db, err := sqlx.Open("postgres", secret.Decrypt(key, dsn))
 	if err != nil {
 		log.Panicln(err)
 	}
