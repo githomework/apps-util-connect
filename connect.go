502a748a718c6bd2e4308a3d443d985d52c0b3e1
David Lam <david.lam@winc.com.au>
2020-10-06 23:00:11 +1100

    commit

---
+++ connect.go
@@ -0,0 +1,78 @@
+package connect
+
+import (
+	"log"
+
+	"github.com/SAP/go-hdb/driver"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-oci8"
+)
+
+// "<user name>/<password>@<server name>:1521/<service name>"
+func Oracle(dsn string) *sqlx.DB {
+	db, err := sqlx.Open("oci8", dsn)
+	if err != nil {
+		log.Println(err)
+	}
+	return db
+}
+
+func OracleMust(dsn string) *sqlx.DB {
+	db, err := sqlx.Open("oci8", dsn)
+	if err != nil {
+		log.Panicln(err)
+	}
+	return db
+}
+
+// "hdb://<user name>:<password>@<server name>:<port>"
+func HANA(dsn string) *sqlx.DB {
+	db, err := sqlx.Open(driver.DriverName, dsn)
+	if err != nil {
+		log.Println(err)
+	}
+	return db
+}
+
+func HANAMust(dsn string) *sqlx.DB {
+	db, err := sqlx.Open(driver.DriverName, dsn)
+	if err != nil {
+		log.Panicln(err)
+	}
+	return db
+}
+
+// "server=<server name>;user id=<user name>;password=<password>;port=1433;database=<database name>;encrypt=disable"
+func MSSQL(dsn string) *sqlx.DB {
+	db, err := sqlx.Open("mssql", dsn)
+	if err != nil {
+		log.Println(err)
+	}
+	return db
+}
+
+func MSSQLMust(dsn string) *sqlx.DB {
+	db, err := sqlx.Open("mssql", dsn)
+	if err != nil {
+		log.Panicln(err)
+	}
+	return db
+}
+
+func Postgres(dsn string) *sqlx.DB {
+	db, err := sqlx.Open("postgres", dsn)
+	if err != nil {
+		log.Println(err)
+	}
+	return db
+}
+
+func PostgresMust(dsn string) *sqlx.DB {
+	db, err := sqlx.Open("postgres", dsn)
+	if err != nil {
+		log.Panicln(err)
+	}
+	return db
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/githomework/apps-util-connect
+
+go 1.15
+
+require (
+	github.com/SAP/go-hdb v0.100.14
+	github.com/denisenkom/go-mssqldb v0.0.0-20200910202707-1e08a3fab204
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.0.0
+	github.com/mattn/go-oci8 v0.0.8
+	google.golang.org/appengine v1.6.6 // indirect
+)
